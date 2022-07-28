585ed2bd9f666764a6aff083da96d3cea88af76e
Christine <cbaba@digitalpharmacist.com>
2022-07-28 12:36:35 -0400

    fix creds

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -4,7 +4,7 @@ var inquirer = require('inquirer');
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
-    user: "root", //Your username
+    user: "", //Your username eg root
     password: "", //Your password
     database: "Bamazon"
 })
